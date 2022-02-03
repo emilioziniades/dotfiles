8d5c216f6f9107e59b3c33e9bcd3e9517db1e180
Emilio Ziniades <emilioziniades@protonmail.com>
2022-02-04 00:02:49 +0200

    added Comment.nvim, updated comment mappings, converted ':' to '<CMD>'

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -13,11 +13,11 @@ function imap(shortcut, command)
 end
 
 -- tab, buffer and window navigation
-nmap('<leader>p', ':tabn<CR>')
-nmap('<leader>o', ':tabp<CR>')
+nmap('<leader>p', '<CMD>tabn<CR>')
+nmap('<leader>o', '<CMD>tabp<CR>')
 
-nmap('<leader>i', ':bn<CR>')
-nmap('<leader>u', ':bp<CR>')
+nmap('<leader>i', '<CMD>bn<CR>')
+nmap('<leader>u', '<CMD>bp<CR>')
 
 nmap('<leader>h', '<C-w>h')
 nmap('<leader>j', '<C-w>j')
@@ -25,17 +25,17 @@ nmap('<leader>k', '<C-w>k')
 nmap('<leader>l', '<C-w>l')
 
 -- quit shortcuts
-nmap('<leader>q', ':qa!<CR>')
-nmap('<leader>w', ':wqa!<CR>')
+nmap('<leader>q', '<CMD>qa!<CR>')
+nmap('<leader>w', '<CMD>wqa!<CR>')
 
 -- comment and uncomment line
-nmap('<leader>/', ':s:^://<CR> :nohlsearch<CR>')
-nmap('<leader>.', ':s:^\\s*//::e<CR> :nohlsearch<CR>')
+nmap('<leader>/', "<CMD>lua require('Comment.api').call('toggle_current_linewise_op')<CR>g@$")
+map('n', '<leader>c', '<CMD>lua require("Comment.api").call("toggle_linewise_op")<CR>g@')
 
 -- remove highlights
-nmap('<leader>,', ':nohlsearch<CR>')
+nmap('<leader>,', '<CMD>nohlsearch<CR>')
 -- terminal shortcut
-nmap('<leader>t', ':bo vs term://zsh<CR> :vert res -20<CR>')
+nmap('<leader>t', '<CMD>bo vs term://zsh<CR> :vert res -20<CR>')
 
 -- easier escape keys
 local esc = "§"
@@ -46,4 +46,4 @@ map('v', esc, '<Esc>')
 map('c', esc, '<C-C><Esc>')
 
 -- plugin-relevant mappings
-nmap('<C-n>', ':NvimTreeToggle<CR>')
+nmap('<C-n>', '<CMD>NvimTreeToggle<CR>')
