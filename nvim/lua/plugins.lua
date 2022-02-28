7ce9df82d8025ce196da18a67300ff8910925ca9
Emilio Ziniades <emilioziniades@gmail.com>
2022-02-28 15:46:18 +0200

    added spelling, lsp.lua, and switched from vim-plug to packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,40 +1,42 @@
 vars = require('variables')
 
---vim plug
 
--- auto install vim plug if not already installed
-config_dir = vim.fn.stdpath('config')
-if vim.fn.empty(vim.fn.glob(config_dir .. '/autoload/plug.vim')) == 1
-	then
-		os.execute('curl -fLo ' .. config_dir .. '/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1' )
-		vim.cmd('autocmd VimEnter * PlugInstall --sync ')
+-- Packer - plugins
+
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
---
-local Plug = vim.fn['plug#']
+vim.cmd [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
 
-vim.call('plug#begin', config_dir .. '/plugged')
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    --functionality
+    use{ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } }
+    use {'nvim-treesitter/nvim-treesitter', run =  ':TSUpdate'} 
+    use 'p00f/nvim-ts-rainbow'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'nvim-lualine/lualine.nvim'
+    use 'numToStr/Comment.nvim'
+    use 'junegunn/goyo.vim'
+    use {'fatih/vim-go', run = ':GoUpdateBinaries' }
+    --themes
+    use 'tanvirtin/monokai.nvim'
+    use 'Mofiqul/dracula.nvim'
+    use 'sjl/badwolf'
+    use 'folke/tokyonight.nvim'
+    use {'rose-pine/neovim', as = 'rose-pine' }
 
--- themes
-Plug('tanvirtin/monokai.nvim')
-Plug('NLKNguyen/papercolor-theme')
-
--- functionality
-Plug('fatih/vim-go', { ['do'] = ':GoUpdateBinaries' })
-Plug('kyazdani42/nvim-tree.lua')
-Plug('kyazdani42/nvim-web-devicons')
-Plug('nvim-lualine/lualine.nvim')
-Plug('numToStr/Comment.nvim')
-Plug('junegunn/goyo.vim')
-
-vim.call('plug#end')
-
--- vim-go settings
-vim.g.go_highlight_functions = 1
-vim.g.go_highlight_function_calls = 1
-vim.g.go_highlight_function_parameters = 0 
-vim.g.go_highlight_types = 0
-vim.g.go_fold_enable = {'block', 'import', 'varconst', 'package_comment', 'comment'}
+end)
 
 -- nvim-tree configuration 
 require'nvim-tree'.setup()
@@ -44,5 +46,44 @@ require('lualine').setup{
     options = { theme = vars.lualinetheme }
 }
 
+
+--nvim-treesitter configuration
+require'nvim-treesitter.configs'.setup{
+    highlight = {
+        enable = true,
+    },
+    rainbow = {
+        enable = true,
+    }, 
+    
+}
+vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
+
+
 -- Comment.nvim configuration
 require('Comment').setup()
+
+-- null-ls 
+
+local null_ls = require('null-ls')
+
+local sources = { 
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.prettier,
+
+}
+null_ls.setup({ 
+    sources = sources,
+     on_attach = function(client)
+            if client.resolved_capabilities.document_formatting then
+                vim.cmd([[
+                augroup LspFormatting
+                    autocmd! * <buffer>
+                    autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+                augroup END
+                ]])
+            end
+        end,
+})
+
