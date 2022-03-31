0e6d97be8d1870e706496b5c29c9d34dcd00105c
Emilio Ziniades <emilioziniades@gmail.com>
2022-03-31 22:16:25 +0200

    nvim: removed float borders, added dark floats for tokyonight

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -1,12 +1,14 @@
 local variables = {}
 local t = os.date("*t")
 if 5 < t.hour and t.hour < 17 then
-	variables.background = "dark"
+	variables.background = "light"
 	variables.theme = "tokyonight"
+	vim.g.tokyonight_style = "day"
 	variables.lualinetheme = "auto"
 else
 	variables.background = "dark"
 	variables.theme = "tokyonight"
+	vim.g.tokyonight_style = "night"
 	variables.lualinetheme = "auto"
 end
 return variables
