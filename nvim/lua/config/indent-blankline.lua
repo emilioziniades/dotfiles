c6bc281b8043add3dc809ff5e46394718e3d6383
Emilio Ziniades <emilioziniades@gmail.com>
2022-04-04 14:17:15 +0200

    nvim: more plugin config

--- nvim/lua/config/indent-blankline.lua
+++ nvim/lua/config/indent-blankline.lua
@@ -1,3 +1,8 @@
+require("indent_blankline").setup({
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = false,
+})
 vim.g.indent_blankline_char = "┊"
 vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
