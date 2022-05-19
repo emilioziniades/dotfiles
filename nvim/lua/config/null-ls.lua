3c336cb940f371c47253436ad415d8c69ba7e2cd
Emilio Ziniades <emilioziniades@gmail.com>
2022-05-19 12:26:02 +0200

    minor changes to nvim config

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.formatting.black,
-	null_ls.builtins.diagnostics.mypy,
+	-- null_ls.builtins.diagnostics.mypy,
 	null_ls.builtins.formatting.goimports,
 	null_ls.builtins.formatting.prettier,
 	null_ls.builtins.formatting.stylua,
