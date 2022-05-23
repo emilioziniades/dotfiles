30ddace75e55881b1ba609e9819625b94d9d6fe9
Emilio Ziniades <emilioziniades@protonmail.com>
2022-05-23 11:01:21 +0200

    nvim: tried to add gore to iron.nvim

--- nvim/lua/config/iron.lua
+++ nvim/lua/config/iron.lua
@@ -1,23 +1,38 @@
 local iron = require("iron.core")
 
-iron.setup {
-config = {
-    highlight_last = "IronLastSent",
-    repl_open_cmd = "vertical bo split",
-    should_map_plug = false,
-    scratch_repl = true,
-    repl_definition = {
-        python = require("iron.fts.python").ipython
-    }
-    },
-    keymaps = {
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_line = "<space>sl",
-        repeat_cmd = "<space>s.",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
-    }
-}
+iron.setup({
+	config = {
+		highlight_last = "IronLastSent",
+		repl_open_cmd = "vertical bo split",
+		should_map_plug = false,
+		scratch_repl = true,
+		repl_definition = {
+			python = require("iron.fts.python").ipython,
+			-- go = { gore = { command =  "gore"  } },
+		},
+	},
+	keymaps = {
+		send_motion = "<space>sc",
+		visual_send = "<space>sc",
+		send_line = "<space>sl",
+		repeat_cmd = "<space>s.",
+		cr = "<space>s<cr>",
+		interrupt = "<space>s<space>",
+		exit = "<space>sq",
+		clear = "<space>cl",
+	},
+})
+
+-- iron.add_repl_definitions {
+--   go = {
+--     gore = {
+--       command = {"gore"}
+--     }
+--   }
+-- }
+--
+-- iron.set_config {
+--   preferred = {
+--     go = "gore",
+--   }
+-- }
