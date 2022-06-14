afdb62742ea316d53210a1d215307d86c259828e
Emilio Ziniades <emilioziniades@gmail.com>
2022-06-15 00:54:19 +0200

    minor changes:
    nvim - improved settings and variables config
    iterm - reduced font-size to 15

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -4,32 +4,35 @@ local vars = require("variables")
 local utils = require("utils")
 
 -- theme (time dependent)
-vim.g.tokyonight_dark_float = true
-vim.g.tokyonight_style = "night"
 vim.cmd("colorscheme " .. vars.theme)
-vim.o.background = vars.background
 
 -- other settings
 
 vim.g.mapleader = " "
 
-vim.o.mouse = "a"
-vim.opt.undofile = true
-vim.o.wrap = true
-vim.o.linebreak = true
-vim.o.spell = false
-vim.o.spelllang = "en_gb"
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.expandtab = true
-vim.o.foldmethod = "expr"
-vim.o.foldenable = false
-vim.o.showmode = false
-vim.o.termguicolors = true
-vim.o.splitright = true
+local options = {
+	background = vars.background,
+	mouse = "a",
+	undofile = true,
+	wrap = true,
+	linebreak = true,
+	spell = false,
+	spelllang = "en_gb",
+	number = true,
+	relativenumber = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	softtabstop = 4,
+	expandtab = true,
+	foldmethod = "expr",
+	foldenable = false,
+	showmode = false,
+	termguicolors = true,
+	splitright = true,
+}
 
+for key, value in pairs(options) do
+	vim.o[key] = value
+end
 -- make current line number pop
 utils.line_number_emphasize()
