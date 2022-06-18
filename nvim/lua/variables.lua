28bf57a0544f7a80ffe83ab44ead19de74ea45f9
Emilio Ziniades <emilioziniades@protonmail.com>
2022-06-19 01:02:37 +0200

    nvim: changed theme to sonokai

--- nvim/lua/variables.lua
+++ nvim/lua/variables.lua
@@ -1,14 +1,14 @@
 local variables = {}
 local t = os.date("*t")
 if 5 < t.hour and t.hour < 17 then
-	variables.theme = "tokyonight"
+	variables.theme = "sonokai"
 	variables.background = "dark"
 	vim.g.tokyonight_style = "storm"
 	-- variables.background = "light"
 	-- vim.g.tokyonight_style = "day"
 	variables.lualinetheme = "auto"
 else
-	variables.theme = "tokyonight"
+	variables.theme = "sonokai"
 	variables.background = "dark"
 	vim.g.tokyonight_style = "night"
 	variables.lualinetheme = "auto"
