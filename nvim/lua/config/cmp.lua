e5f306a8963575ba8176ed29e78f7d8f53fe57c7
Emilio Ziniades <emilioziniades@gmail.com>
2022-06-19 01:03:01 +0200

    nvim: improved telescope and cmp config

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -12,19 +12,17 @@ cmp.setup({
 		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-s>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp", keyword_length = 3 },
+		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "nvim_lua" },
-		{ name = "buffer", keyword_length = 3 },
+		{ name = "buffer" },
 		{ name = "path" },
 		{ name = "buffer" },
 		-- { name = "name", keyword_length = n, max_item_count = n,  }
