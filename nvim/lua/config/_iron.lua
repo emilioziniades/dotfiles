2cae07ec5cd64cf3bd453dfe4710b3b11ce490b0
Emilio Ziniades <emilioziniades@protonmail.com>
2022-07-12 22:40:23 +0200

    nvim: iron config improved

--- nvim/lua/config/_iron.lua
+++ nvim/lua/config/_iron.lua
@@ -1,14 +1,24 @@
 local iron = require("iron.core")
+local bracketed_paste = require("iron.fts.common").bracketed_paste
+
+-- copied from iron.fts.python
+local def = function(cmd)
+	return {
+		command = cmd,
+		format = bracketed_paste,
+	}
+end
 
 iron.setup({
 	config = {
-		highlight_last = "IronLastSent",
+		highlight_last = false,
 		repl_open_cmd = "vertical bo split",
 		should_map_plug = false,
 		scratch_repl = true,
+		buflisted = true,
 		repl_definition = {
-			python = require("iron.fts.python").ipython,
-			-- go = { gore = { command =  "gore"  } },
+			-- python = require("iron.fts.python").ipython,
+			python = def({ "ipython", "--no-autoindent", "--no-confirm-exit" }),
 		},
 	},
 	keymaps = {
@@ -22,19 +32,6 @@ iron.setup({
 	},
 })
 
--- iron.add_repl_definitions {
---   go = {
---     gore = {
---       command = {"gore"}
---     }
---   }
--- }
---
--- iron.set_config {
---   preferred = {
---     go = "gore",
---   }
--- }
 local function run_file_repl()
 	require("iron").core.send(vim.bo.ft, vim.fn.readfile(vim.fn.expand("%")))
 end
@@ -45,3 +42,4 @@ end
 
 vim.keymap.set("n", "<leader>rr", run_file_repl)
 vim.keymap.set("n", "<leader>rs", start_repl)
+vim.keymap.set("n", "<leader>rt", iron.repl_restart)
