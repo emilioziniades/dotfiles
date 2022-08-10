bce19cf82aaf814b92964e5ae159468da987054e
Emilio Ziniades <emilioziniades@protonmail.com>
2022-08-10 22:06:45 +0200

    initial clean and tidy

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,9 +1,9 @@
 require("lualine").setup({
-    options = {
-        icons_enabled = false,
-        theme = require("variables").lualinetheme,
-        component_separators = "|",
-        section_separators = "",
-        globalstatus = true,
-    },
+	options = {
+		icons_enabled = false,
+		theme = "auto",
+		component_separators = "|",
+		section_separators = "",
+		globalstatus = true,
+	},
 })
---
+++ nvim/lua/config/packer.lua
@@ -0,0 +1,3 @@
+
+vim.keymap.set("n", "<leader>ps", "<cmd>PackerSync<cr>")
+vim.keymap.set("n", "<leader>pc", "<cmd>PackerCompile<cr>")
