636d8dbeddb8ec3532ed5ab45c94ec61d1ec5f09
Emilio Ziniades <emilioziniades@protonmail.com>
2022-11-04 21:36:03 +0200

    nvim: telescope keymap for explorer cwd

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -16,6 +16,10 @@ local function find_dotfiles()
 	require("telescope.builtin").find_files({ cwd = config_dir })
 end
 
+local function file_browser_cwd()
+	require("telescope").extensions.file_browser.file_browser({ path = vim.fn.expand("%:p:h") })
+end
+
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers)
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
@@ -23,5 +27,6 @@ vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
 vim.keymap.set("n", "<leader>fo", require("telescope.builtin").oldfiles)
 vim.keymap.set("n", "<leader>fb", require("telescope.builtin").current_buffer_fuzzy_find)
 vim.keymap.set("n", "<leader>fc", require("telescope.builtin").colorscheme)
-vim.keymap.set("n", "<leader>fe", require("telescope").extensions.file_browser.file_browser)
+vim.keymap.set("n", "<leader>fp", require("telescope").extensions.file_browser.file_browser)
+vim.keymap.set("n", "<leader>fe", file_browser_cwd)
 vim.keymap.set("n", "<leader>fd", find_dotfiles)
