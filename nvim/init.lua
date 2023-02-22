2fee4b9b0a7719819e3772a9bffa9579e1e9c740
Emilio Ziniades <emilioziniades@protonmail.com>
2023-02-22 23:23:30 +0200

    delete unused function

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,39 +18,6 @@ local function toggle_relative_line_numbers()
 	end
 end
 
-local function line_number_emphasize()
-	local line_num_colour
-	if vim.o.background == "light" then
-		line_num_colour = "Black"
-	else
-		line_num_colour = "White"
-	end
-	vim.o.cursorline = true
-	vim.cmd("hi Cursorline guibg=none")
-	vim.cmd("hi CursorLineNr term=bold ctermfg=" .. line_num_colour .. " gui=bold guifg=" .. line_num_colour)
-end
-
-local function toggle_theme_background()
-	local colorscheme = vim.cmd("silent colorscheme")
-	if vim.o.background == "dark" then
-		vim.o.background = "light"
-	else
-		vim.o.background = "dark"
-	end
-	vim.cmd("silent colorscheme " .. colorscheme)
-	-- utils.line_number_emphasize()
-end
-
-local function count_words()
-	if vim.fn.wordcount().visual_words == 1 then
-		print(vim.fn.wordcount().visual_words .. " word selected")
-	elseif not (vim.fn.wordcount().visual_words == nil) then
-		print(vim.fn.wordcount().visual_words .. " words selected")
-	else
-		print(vim.fn.wordcount().words .. " words")
-	end
-end
-
 local function run_file()
 	local ft = vim.bo.filetype
 	local run_cmds = {
