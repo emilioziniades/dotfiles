a10666687eb2511da330db070fb510be283b2cf2
Emilio Ziniades <emilioziniades@protonmail.com>
2023-02-22 23:26:02 +0200

    fix zen mode settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -608,17 +608,6 @@ require("packer").startup(function(use)
 						relativenumber = false,
 					},
 				},
-				plugins = {
-					-- disable some global vim options (vim.o...)
-					-- comment the lines to not apply the options
-					options = {
-						enabled = true,
-						ruler = false, -- disables the ruler text in the cmd line area
-						showcmd = false, -- disables the command in the last line of the screen
-					},
-					gitsigns = { enabled = false }, -- disables git signs
-					tmux = { enabled = false }, -- disables the tmux statusline
-				},
 			})
 			Map("n", "<leader>zz", require("zen-mode").toggle)
 		end,
