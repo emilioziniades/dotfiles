4b0c27c89fa7786f3eaa1b4820f6f3071d35878e
Emilio Ziniades <emilioziniades@gmail.com>
2023-02-22 21:02:47 +0200

    more consolidate into init.lua

--- nvim/lua/config/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-require("indent_blankline").setup({
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = false,
-})
-vim.g.indent_blankline_char = "┊"
-vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_show_trailing_blankline_indent = false
