1b567f88e5421431da662bc2788b24e044c15585
Emilio Ziniades <emilioziniades@protonmail.com>
2023-06-29 21:46:05 +0200

    start migrating to alacritty

--- flake.nix
+++ flake.nix
@@ -11,25 +11,30 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
-    let system = "x86_64-darwin"; in {
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-darwin";
+  in {
+    defaultPackage.${system} = home-manager.defaultPackage.${system};
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
 
-      homeConfigurations = {
-        "emilioziniades" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            ./home.nix
-            {
-              home = {
-                username = "emilioziniades";
-                homeDirectory = "/Users/emilioziniades";
-                stateVersion = "23.11";
-              };
-            }
-          ];
-        };
+    homeConfigurations = {
+      "emilioziniades" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home.nix
+          {
+            home = {
+              username = "emilioziniades";
+              homeDirectory = "/Users/emilioziniades";
+              stateVersion = "23.11";
+            };
+          }
+        ];
       };
     };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,5 +1,34 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.home-manager.enable = true;
   home.packages = [
+    pkgs.alacritty
+    pkgs.fd
+    pkgs.tree
+    pkgs.ripgrep
+    pkgs.jq
+    pkgs.diffutils
+    pkgs.wget
+    pkgs.alejandra
   ];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      import = ["~/dotfiles/colours/catppuccin/alacritty/catppuccin-mocha.yml"];
+      env = {
+        TERM = "xterm-256color";
+      };
+      window.padding = {
+        x = 10;
+        y = 10;
+      };
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font Mono"; #TODO: install this nerdfont
+          style = "Medium";
+        };
+        size = 14.0;
+      };
+    };
+  };
 }
