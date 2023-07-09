2ca10fc7ac5880ccf36d1d02f5ade0d0bbedc4cf
Emilio Ziniades <emilioziniades@protonmail.com>
2023-07-09 12:34:03 +0200

    BIG TIDY: remove bash scripts for setup

--- bin/install_dotnet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual
-set -e
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-sudo chmod +x ./dotnet-install.sh
-# install both net6 and net7
-./dotnet-install.sh --channel 7.0
-./dotnet-install.sh --channel 6.0
-rm ./dotnet-install.sh
