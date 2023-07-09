250fb9aa8e5b04f14feba2837d802bc1ca4cc600
Emilio Ziniades <emilioziniades@protonmail.com>
2023-07-09 11:37:44 +0200

    tidy nix cfg

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,8 @@
     ];
   };
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   nixpkgs.config = {
     allowUnfree = true;
     permittedInsecurePackages = [
--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -3,6 +3,8 @@
   pkgs,
   ...
 }: {
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   nixpkgs.config = {
     allowUnfree = true;
     permittedInsecurePackages = [
