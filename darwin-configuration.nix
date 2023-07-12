ad84a7adf41f934edfe36c6106d2ad5c5da6c773
Emilio Ziniades <emilioziniades@protonmail.com>
2023-07-12 18:47:21 +0200

    update nix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -49,11 +49,10 @@
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
+  programs.zsh = {
+    enable = true;
+    promptInit = "";
+  };
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
   system.stateVersion = 4;
 }
