ab21e54515c7e786a345b2381feed0331c85a721
Emilio Ziniades <emilioziniades@protonmail.com>
2023-12-15 14:49:56 +0200

    nix: fold zshrc into home.nix

--- home.nix
+++ home.nix
@@ -152,8 +152,26 @@
       save = 10000;
       size = 10000;
     };
-    initExtra = builtins.readFile ./zshrc;
     syntaxHighlighting.enable = true;
+    initExtra = ''
+      # if you want to profile zsh startup times, see: https://esham.io/2018/02/zsh-profiling
+
+      PROMPT='%F{yellow}%~%f %F{blue}%#%f '
+
+      bindkey '^[[1;5D' backward-word
+      bindkey '^[[1;5C' forward-word
+
+      export GPG_TTY=$(tty)
+
+      export GOPATH=$HOME/go/bin
+      export PATH="$GOPATH:$PATH"
+
+      export DOTNET_ROOT=$HOME/.dotnet
+      export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+      export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+      [[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
+    '';
   };
 
   programs.bash = {
