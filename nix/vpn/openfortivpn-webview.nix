2e980ec0701761cb6efc4fd1241221039cfc56cc
Emilio Ziniades <emilioziniades@protonmail.com>
2024-01-13 16:53:35 +0200

    nix: openfortivpn-webview build

--- nix/vpn/openfortivpn-webview.nix
+++ nix/vpn/openfortivpn-webview.nix
@@ -1,1 +1,28 @@
-{pkgs}: pkgs.writeScriptBin "openfortivpn-webview" ''echo "fek"''
+{pkgs}:
+pkgs.stdenv.mkDerivation {
+  pname = "openfortivpn-webview";
+  version = "1.1.2-electron";
+  src = pkgs.fetchFromGitHub {
+    owner = "gm-vm";
+    repo = "openfortivpn-webview";
+    rev = "607dc949730f24611a6dba6c5c6cb9a5669fd7ac";
+    sha256 = "sha256-SZaC5bN2cYaMIOhqxisd3AXqKO4P/kmBcbg0IaEflr4=";
+  };
+  nativeBuildInputs = with pkgs; [
+    libsForQt5.qt5.qtwebengine
+    libsForQt5.qt5.qtbase
+    libsForQt5.qt5.wrapQtAppsHook
+    gnumake
+    gcc
+  ];
+  buildPhase = ''
+    cd openfortivpn-webview-qt
+    qmake .
+    make
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    # cp openfortivpn-webview /$out/bin
+    cp ./openfortivpn-webview.app/Contents/MacOS/openfortivpn-webview /$out/bin
+  '';
+}
