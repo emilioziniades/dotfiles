c85a0599181f682ec8251160c1ed22e250c0459d
Emilio Ziniades <emilioziniades@gmail.com>
2024-01-14 23:15:34 +0200

    nix: openfortivpn-webview WIP

--- nix/vpn/openfortivpn-webview.nix
+++ nix/vpn/openfortivpn-webview.nix
@@ -1,28 +1,70 @@
 {pkgs}:
-pkgs.stdenv.mkDerivation {
+pkgs.buildNpmPackage rec {
   pname = "openfortivpn-webview";
   version = "1.1.2-electron";
-  src = pkgs.fetchFromGitHub {
+
+  project = pkgs.fetchFromGitHub {
     owner = "gm-vm";
     repo = "openfortivpn-webview";
-    rev = "607dc949730f24611a6dba6c5c6cb9a5669fd7ac";
-    sha256 = "sha256-SZaC5bN2cYaMIOhqxisd3AXqKO4P/kmBcbg0IaEflr4=";
+    rev = "v${version}";
+    sha256 = "sha256-BNotbb2pL7McBm0SQwcgEvjgS2GId4HVaxWUz/ODs6w=";
   };
+  src = "${project}/openfortivpn-webview-electron";
+
+  npmDepsHash = "sha256-FvonIgVWAB0mHQaYcJkrZ9pn/nrTju2Br5OkmtGFsIk";
+
   nativeBuildInputs = with pkgs; [
-    libsForQt5.qt5.qtwebengine
-    libsForQt5.qt5.qtbase
-    libsForQt5.qt5.wrapQtAppsHook
-    gnumake
-    gcc
+    makeWrapper
   ];
-  buildPhase = ''
-    cd openfortivpn-webview-qt
-    qmake .
-    make
+
+  buildInputs = [
+  ];
+
+  # postPatch = ''
+  #   substituteInPlace scripts/build-.sh \
+  #     --replace 'if [ "$1" == "snap" ]; then' 'exit 0; if [ "$1" == "snap" ]; then'
+  # '';
+
+  ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
+  electron = pkgs.electron;
+
+  dontNpmBuild = true;
+  postBuild = ''
+    # npm run build --workspace=packages/bruno-graphql-docs
+    # npm run build --workspace=packages/bruno-app
+    # npm run build --workspace=packages/bruno-query
+
+    # bash scripts/build-.sh
+
+    # pushd packages/bruno-
+    # ls -aR {electron}
+    # echo "FUCK"
+    # exit 1
+
+    npm exec -builder -- \
+      --dir \
+      -c.Dist=${electron}/bin/electron \
+      -c.Version=${electron.version} \
+      -c.npmRebuild=false
+
+    # popd
   '';
+
+  # npmPackFlags = ["--ignore-scripts"];
+
   installPhase = ''
-    mkdir -p $out/bin
-    # cp openfortivpn-webview /$out/bin
-    cp ./openfortivpn-webview.app/Contents/MacOS/openfortivpn-webview /$out/bin
+    runHook preInstall
+
+    echo "WE GOT HERE MOTHERFLUFFER"
+    exit 1
+
+    # mkdir -p $out/opt/bruno $out/bin
+    #
+    # cp -r packages/bruno-/dist/linux-unpacked/{locales,resources{,.pak}} $out/opt/bruno
+    #
+    # makeWrapper $lib.getExe  $out/bin/bruno \
+    #   --add-flags $out/opt/bruno/resources/app.asar \
+    #
+    # runHook postInstall
   '';
 }
