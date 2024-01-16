39fbda923e45cb8f6bc35554c6dd62a7c581ecd7
Emilio Ziniades <emilioziniades@protonmail.com>
2024-01-16 22:27:50 +0200

    nix: switch to new shiny vpn script

--- nix/vpn/vpn.sh
+++ nix/vpn/vpn.sh
@@ -1,74 +1,67 @@
 #!/usr/bin/env bash
+
+# A CLI tool for connecting to Fortinet VPN via SAML login.
+#
+# Essentially a script to glue [`openfortivpn`](https://github.com/adrienverge/openfortivpn)
+# and [`openfortivpn-webview`](https://github.com/gm-vm/openfortivpn-webview) together.
+#
+# This tool makes heavy use of sudo, which is needed for openfortivpn
+# to do it's job. Vet this script (and it's dependencies) before running
+# on your machine.
+#
+# You will need to create a configuration file at ~/.vpn. This is a JSON
+# array with "name", "host", "port" and "default" fields. An example
+# configuration file looks like this:
+#
+# [
+#     {
+#         "name": "favourite-vpn",
+#         "host":  "one.vpn.com",
+#         "port":  1234,
+#         "default": true
+#     },
+#     {
+#         "name": "less-favourite-vpn",
+#         "host":  "two.vpn.com",
+#         "port":  1234,
+#         "default": false
+#     }
+# ]
+#
+# Then you can run `vpn less-favourite-vpn` or `vpn` to use the
+# default.
+#
+# It is possible to setup this script to run as a systemd service,
+# but I usually just run the script, hit ctrl-z and then the `bg` command.
 set -e
 
 VPN_CFG_FILE=~/.vpn
-VPN_NAME_FILE=~/.vpn.current
 VPN_PID=$(pgrep openfortivpn || true)
 
-main() {
-	if [[ ! -e $VPN_CFG_FILE ]]; then
-		echo "please create a configuration file at ~/.vpn with at least one default"
-		exit 1
-	fi
+if [[ ! -e $VPN_CFG_FILE ]]; then
+	echo "please create a configuration file at ~/.vpn with at least one default"
+	exit 1
+fi
 
-	case $1 in
-	up)
-		up $2
-		;;
-	down)
-		down
-		;;
-	status)
-		status
-		;;
-	logs)
-		logs
-		;;
-	*)
-		echo "please specify a valid subcommand: up {vpn-name}, down, status"
-		;;
-	esac
-}
+if [[ -n $VPN_PID ]]; then
+	echo "vpn already connected"
+	exit 1
+fi
 
-up() {
-	if [[ -z $VPN_PID ]]; then
-		if [[ -z $1 ]]; then
-			echo "connecting to default vpn"
-			host=$(jq -e -c 'map(select(.default == true)) | "\(.[0].host):\(.[0].port)"' $VPN_CFG_FILE | tr -d '"')
-			name=$(jq -e -c 'map(select(.default == true)) | .[0].name' $VPN_CFG_FILE | tr -d '"')
-		else
-			host=$(jq -e -c "map(select(.name == \"$1\")) | \"\(.[0].host):\(.[0].port)\"" $VPN_CFG_FILE | tr -d '"')
-			name=$(jq -e -c "map(select(.name == \"$1\")) | .[0].name" $VPN_CFG_FILE | tr -d '"')
-			if [[ $? == 1 ]]; then
-				echo "could not find host $1, check the ~/.vpn config file"
-				exit 1
-			fi
-		fi
-		echo "connecting to $name"
-		echo $name >$VPN_NAME_FILE
-		COOKIE=$(openfortivpn-webview $host)
-		sudo -b openfortivpn --cookie=$COOKIE $host
-	else
-		echo "vpn already connected"
-	fi
-}
+if [[ -z $1 ]]; then
+	filter="map(select(.default == true))"
+else
+	filter="map(select(.name == \"$1\"))"
+fi
 
-down() {
-	if [[ -z $VPN_PID ]]; then
-		echo "vpn already down"
-	else
-		echo "stopping vpn"
-		sudo kill $VPN_PID
-		rm $VPN_NAME_FILE
-	fi
-}
+host=$(jq -e -c "$filter | \"\(.[0].host):\(.[0].port)\"" $VPN_CFG_FILE | tr -d '"')
+name=$(jq -e -c "$filter | .[0].name" $VPN_CFG_FILE | tr -d '"')
 
-status() {
-	if [[ -z $VPN_PID ]]; then
-		echo "[DOWN]"
-	else
-		echo "[UP] [PID: $VPN_PID] [VPN: $(cat $VPN_NAME_FILE)]"
-	fi
-}
+if [[ $? == 1 ]]; then
+	echo "could not find host $1, check the ~/.vpn config file"
+	exit 1
+fi
 
-main $1 $2
+echo "connecting to $name"
+COOKIE=$(openfortivpn-webview $host)
+sudo --preserve-env=$PATH openfortivpn --cookie=$COOKIE $host
