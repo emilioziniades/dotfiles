037fcaf9bb1c7b5ebb3f0e6479707071e47281ef
Emilio Ziniades <emilioziniades@gmail.com>
2024-01-17 21:11:30 +0200

    nix + nvim: remove mason in favour of nix

---
+++ nix/pkgs/csharpier/package.nix
@@ -0,0 +1,7 @@
+{buildDotnetGlobalTool}:
+buildDotnetGlobalTool {
+  pname = "csharpier";
+  version = "0.26.4";
+  executables = "dotnet-csharpier";
+  nugetSha256 = "sha256-9ieSAWulfJZz0INRf95Nz5C865sM8kffK2UGZ1NZ6Z0=";
+}
