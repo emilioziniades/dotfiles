05be3bca56575cefc0fb548171bdcaa3e18a9f84
Emilio Ziniades <emilioziniades@gmail.com>
2024-01-21 19:23:07 +0200

    nix: use upstream for nixos/nixpkgs#272806

--- nix/pkgs/csharpier/package.nix
+++
@@ -1,7 +0,0 @@
-{buildDotnetGlobalTool}:
-buildDotnetGlobalTool {
-  pname = "csharpier";
-  version = "0.26.4";
-  executables = "dotnet-csharpier";
-  nugetSha256 = "sha256-9ieSAWulfJZz0INRf95Nz5C865sM8kffK2UGZ1NZ6Z0=";
-}
