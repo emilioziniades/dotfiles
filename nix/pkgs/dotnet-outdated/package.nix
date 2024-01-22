d6b0f6f4c1a8b2e6b680c61a3739e72958c622d8
Emilio Ziniades <emilioziniades@protonmail.com>
2024-01-22 11:08:25 +0200

    nix: add dotnet-outdated

---
+++ nix/pkgs/dotnet-outdated/package.nix
@@ -0,0 +1,7 @@
+{buildDotnetGlobalTool}:
+buildDotnetGlobalTool {
+  pname = "dotnet-outdated-tool";
+  version = "4.6.0";
+  executables = "dotnet-outdated";
+  nugetSha256 = "sha256-FcXdgalGHiii2TUUPSqVuG/2DPjNGTN1/2afoU1zKOs=";
+}
