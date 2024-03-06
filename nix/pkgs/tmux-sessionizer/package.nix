448d1ba1b6ecf20d3443c9fdae65eb551e8a59f2
Emilio Ziniades <emilioziniades@protonmail.com>
2024-03-06 22:40:47 +0200

    nix: bump tms

--- nix/pkgs/tmux-sessionizer/package.nix
+++ nix/pkgs/tmux-sessionizer/package.nix
@@ -11,16 +11,16 @@ rustPlatform.buildRustPackage rec {
   name = "tmux-sessionizer";
   pname = name;
 
-  version = "8f753a9baa13ed077f750c85fa4cd2997bf57b09";
+  version = "3611edb8772e5f43ea8b476c6fead012881e1c43";
 
   src = fetchFromGitHub {
     owner = "jrmoulton";
     repo = name;
     rev = version;
-    hash = "sha256-wJ258HPPpjvlzNEWiRwk9IwxPiCrX26MiP83iyQEgDY=";
+    hash = "sha256-Y9uAf7Q+lbkLRIaySmprvEYDIoZ1b61rR+TyEIkIcqE=";
   };
 
-  cargoHash = "sha256-5A/mG+vxuu7wXiVvEbrMCNCLGRQRMv8qmwBJ5m7mZhA=";
+  cargoHash = "sha256-vHJtBEzDzP7jdE0bsmVhUojZITXd0C6j88T6Ku4NQSY=";
 
   # Needed to get openssl-sys to use pkg-config.
   OPENSSL_NO_VENDOR = 1;
