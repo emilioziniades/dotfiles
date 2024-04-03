b01ba9bab74835f4dc0c300a61005d5f0a2cced4
Emilio Ziniades <emilioziniades@protonmail.com>
2024-04-03 21:41:25 +0200

    nix: do tmux config better

--- nix/home.nix
+++ nix/home.nix
@@ -276,23 +276,26 @@
     historyLimit = 100000;
     escapeTime = 10;
     sensibleOnTop = false;
-    plugins = [];
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_text "#W"
+          set -g @catppuccin_window_left_separator "█"
+          set -g @catppuccin_window_right_separator "█"
+          set -g @catppuccin_status_left_separator "█"
+          set -g @catppuccin_status_right_separator "█"
+          set -g @catppuccin_status_modules "session"
+        '';
+      }
+    ];
     extraConfig = ''
       set -ag terminal-overrides ",xterm-256color:RGB"
       # see here for below voodoo: https://github.com/tmux/tmux/issues/1202
       set -as terminal-overrides ',xterm*:sitm=\E[3m'
       set-option -g focus-events on
       set-option -g renumber-windows on
-
-      # manually source catppuccin plugin after options are applied
-      set -g @catppuccin_window_default_text "#W"
-      set -g @catppuccin_window_current_text "#W"
-      set -g @catppuccin_window_left_separator "█"
-      set -g @catppuccin_window_right_separator "█"
-      set -g @catppuccin_status_left_separator "█"
-      set -g @catppuccin_status_right_separator "█"
-      set -g @catppuccin_status_modules "session"
-      run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
 
       bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded ~/.config/tmux/tmux.conf"
       bind j next-window
