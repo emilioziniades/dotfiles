c9a528e17c0178010cdbd666ac6e8e3341e6e79e
Emilio Ziniades <emilioziniades@gmail.com>
2024-04-04 11:09:54 +0200

    nix: add git module, remove starship module

---
+++ nix/modules/home-manager/git.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.ez.programs.git;
+in {
+  options.ez.programs.git = with types; {
+    enable = mkEnableOption "git";
+    email = mkOption {
+      type = str;
+    };
+    username = mkOption {
+      type = str;
+    };
+    gpgKey = mkOption {
+      type = either string bool;
+    };
+    defaultBranch = mkOption {
+      type = str;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userName = cfg.username;
+      userEmail = cfg.email;
+      aliases = {
+        i = "init";
+        s = "status";
+        l = "log --oneline --all";
+        g = "log --oneline --graph --decorate --all";
+        lf = "log";
+        d = "diff";
+        a = "add";
+        c = "commit";
+        p = "push";
+      };
+      extraConfig = {
+        user = {
+          signingkey = cfg.gpgKey;
+        };
+        init = {
+          defaultBranch = cfg.defaultBranch;
+        };
+        core = {
+          editor = "nvim";
+        };
+        commit = {
+          gpgsign = false;
+        };
+        push = {
+          autoSetupRemote = true;
+        };
+        pull = {
+          rebase = false;
+        };
+      };
+    };
+  };
+}
