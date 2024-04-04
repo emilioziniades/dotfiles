f0588ba3367903a61541f54641d77b2283b04c75
Emilio Ziniades <emilioziniades@gmail.com>
2024-04-04 14:40:44 +0200

    nix: make ALL the modules

---
+++ nix/modules/home-manager/infrastructure-as-code.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.ez.programs.infrastructure-as-code = {
+    enable = lib.mkEnableOption "IAC development tools";
+  };
+
+  config = lib.mkIf config.ez.programs.infrastructure-as-code.enable {
+    home.packages = with pkgs; [
+      packer
+      terraform
+      terraform-ls
+    ];
+  };
+}
