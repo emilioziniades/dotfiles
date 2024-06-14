2e65c0201bc5dd6323e21f0046854f25fca53c67
Emilio Ziniades <emilioziniades@protonmail.com>
2024-06-14 21:02:27 +0200

    nix: wezterm on macos with brew (sigh)

--- nix/modules/home-manager/wezterm.nix
+++ nix/modules/home-manager/wezterm.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    wezterm
-  ];
+  # TODO: wezterm is currently broken on macos
+  # so I'm installing it with brew until this is fixed
+  # see: https://github.com/NixOS/nixpkgs/issues/239384
+  home.packages = with pkgs;
+    if stdenv.isLinux
+    then [
+      wezterm
+    ]
+    else [];
 
   xdg.configFile.wezterm = {
     source = ../../../wezterm;
