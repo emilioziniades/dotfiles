afa245cb97ad045390e5ebf7441005943b4ce082
Emilio Ziniades <emilioziniades@protonmail.com>
2024-06-24 10:22:26 +0200

    wezterm: launch tmux on start

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.font = wezterm.font("MonaspiceNe Nerd Font", { weight = "Medium" })
+config.default_prog = { "tmux" }
 
 wezterm.on("format-window-title", function()
 	return "WezTerm"
