a49a5cdc102a845fdb7bd5fd01373675f1959453
Emilio Ziniades <emilioziniades@protonmail.com>
2024-06-26 11:23:01 +0200

    add standalone home-manager output to flake

--- flake.nix
+++ flake.nix
@@ -63,6 +63,20 @@
       ];
     };
 
+    homeConfigurations.emilioz = let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./nix/hosts/kayak/home.nix
+        ];
+        extraSpecialArgs = {
+          inherit nix-std dotfiles-secrets;
+        };
+      };
+
     darwinConfigurations.hadedah = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
       modules = [
