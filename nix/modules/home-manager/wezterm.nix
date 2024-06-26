0bdf801fd62a6cd27f22fa69ba638da166dac5d7
Emilio Ziniades <emilioziniades@gmail.com>
2024-06-26 11:36:42 +0200

    update wezterm for new work machine

--- nix/modules/home-manager/wezterm.nix
+++ nix/modules/home-manager/wezterm.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{
   # TODO: wezterm is currently broken on macos
   # so I'm installing it with brew until this is fixed
   # see: https://github.com/NixOS/nixpkgs/issues/239384
-  home.packages = with pkgs;
-    if stdenv.isLinux
-    then [wezterm]
-    else [];
+  # TODO: wezterm doesn't play nice on non-nixos systems,
+  # so on Debian I am going to just rely on the native package
+  # manager
+  home.packages = [];
 
   xdg.configFile.wezterm = {
     source = ../../../wezterm;
