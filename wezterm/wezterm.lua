bdbfce355d5514e3c8dce41a3ad480e56621b71c
Emilio Ziniades <emilioziniades@protonmail.com>
2024-07-08 13:17:45 +0200

    wezterm: use native macos fullscreen

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,11 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-config.enable_tab_bar = false
 config.font = wezterm.font("MonaspiceNe Nerd Font", { weight = "Medium" })
+
+config.enable_tab_bar = false
+
+config.native_macos_fullscreen_mode = true
 
 wezterm.on("format-window-title", function()
 	return "WezTerm"
