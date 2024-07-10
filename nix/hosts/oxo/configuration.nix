b46a3517689d3269f49deaaa30d7d5a5193850ed
Emilio Ziniades <emilioziniades@protonmail.com>
2024-07-10 20:19:16 +0200

    nix: initial setup for oxo

---
+++ nix/hosts/oxo/configuration.nix
@@ -0,0 +1,44 @@
+{pkgs, ...}: {
+  imports = [
+    ./disko-configuration.nix
+  ];
+
+  networking.hostName = "oxo";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 2;
+
+  # nix configuration
+  nix.extraOptions = "experimental-features = nix-command flakes";
+
+  # setup virtualbox guest additions
+  virtualisation.virtualbox.guest.enable = true;
+
+  time.timeZone = "Africa/Johannesburg";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  users = {
+    mutableUsers = false;
+
+    users.emilioz = {
+      isNormalUser = true;
+      home = "/home/emilioz";
+      extraGroups = ["wheel"];
+      hashedPassword = "$y$j9T$..ZrEKsB5VjpEoeIARpln1$eB8HV5Bo.kQrQtZ88nngNd5i5T4F1F1BuM1ySEIA5J3";
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
