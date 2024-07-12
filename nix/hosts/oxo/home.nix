83f386470778634dfd1df5024088e37e4b98a517
Emilio Ziniades <emilioz@za.velocitytrade.com>
2024-07-12 22:21:19 +0200

    nix: use home-manager for oxo

---
+++ nix/hosts/oxo/home.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ../../modules/home-manager
+  ];
+
+  home.username = "emilioz";
+  home.homeDirectory = "/home/emilioz";
+  home.stateVersion = "24.05";
+
+  ez.programs.neovim.enable = true;
+  ez.programs.tmux.enable = true;
+
+  ez.programs.git = {
+    enable = true;
+    username = "Emilio Ziniades";
+    email = "emilioz@za.velocitytrade.com";
+  };
+}
