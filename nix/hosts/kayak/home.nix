42070237f8f1a100078ab3b7b6cb1c1bf650354f
Emilio Ziniades <emilioziniades@gmail.com>
2024-07-12 22:14:41 +0200

    nix: make most home-manager modules enable-able

--- nix/hosts/kayak/home.nix
+++ nix/hosts/kayak/home.nix
@@ -15,6 +15,11 @@
   # TODO: only do this when we are not on nixos (how do we differentiate between nixos and non-nixos linux?)
   targets.genericLinux.enable = true;
 
+  ez.programs.neovim.enable = true;
+  ez.programs.tmux.enable = true;
+  ez.programs.wezterm.enable = true;
+  ez.programs.tms.enable = true;
+
   ez.programs.git = {
     enable = true;
     username = "Emilio Ziniades";
