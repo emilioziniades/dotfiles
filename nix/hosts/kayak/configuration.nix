d99627d696315df2b0e01561d125836933ae4b84
Emilio Ziniades <emilioziniades@protonmail.com>
2024-09-22 13:44:38 +0200

    nix(kayak): disable wayland for gnome
    I was experiencing screensharing and audio issues with gnome+wayland

--- nix/hosts/kayak/configuration.nix
+++ nix/hosts/kayak/configuration.nix
@@ -16,6 +16,7 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = false;
     desktopManager.gnome.enable = true;
     xkb.options = "caps:backspace";
   };
