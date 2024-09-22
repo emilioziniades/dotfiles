8bb64b851617f8a216fbd42455dff43d03fc8e9b
Emilio Ziniades <emilioziniades@protonmail.com>
2024-09-22 13:40:55 +0200

    nix/wezterm: use wezterm from nixpkgs on kayak

--- nix/modules/home-manager/wezterm.nix
+++ nix/modules/home-manager/wezterm.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -12,10 +13,10 @@ in {
     # TODO: wezterm is currently broken on macos
     # so I'm installing it with brew until this is fixed
     # see: https://github.com/NixOS/nixpkgs/issues/239384
-    # TODO: wezterm doesn't play nice on non-nixos systems,
-    # so on Debian I am going to just rely on the native package
-    # manager
-    home.packages = [];
+    home.packages =
+      if pkgs.stdenv.isLinux
+      then [pkgs.wezterm]
+      else [];
 
     xdg.configFile.wezterm = {
       source = ../../../wezterm;
