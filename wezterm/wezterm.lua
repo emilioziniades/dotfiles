53486f76174446774407c969946cd9fd6330a9b0
Emilio Ziniades <emilioziniades@protonmail.com>
2024-09-25 11:13:41 +0200

    wezterm: update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,16 +3,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("MonaspiceNe Nerd Font", { weight = "Medium" })
-
 config.enable_tab_bar = false
-
+config.audible_bell = "Disabled"
 config.native_macos_fullscreen_mode = true
+
+--TODO: Keep this here until this issue is resolved: https://github.com/wez/wezterm/issues/5990
+config.front_end = "WebGpu"
 
 wezterm.on("format-window-title", function()
 	return "WezTerm"
 end)
-
---TODO: Keep this here until this issue is resolved: https://github.com/wez/wezterm/issues/5990
-config.front_end = "WebGpu"
 
 return config
