27f11207b54fd0d7528641cc5e3cf9d4d39f8db0
Emilio Ziniades <emilioziniades@protonmail.com>
2024-10-22 18:25:03 +0200

    Revert "wezterm: remove temporary hack"
    This reverts commit e02bac41507180d1cde219d6d9b5bf7563f56525.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,9 @@ config.enable_tab_bar = false
 config.audible_bell = "Disabled"
 config.native_macos_fullscreen_mode = true
 
+--TODO: Keep this here until this issue is resolved: https://github.com/wez/wezterm/issues/5990
+config.front_end = "WebGpu"
+
 wezterm.on("format-window-title", function()
 	return "WezTerm"
 end)
