0a0f8f3ec9378f8d5111ac834becd224cb4d518f
Emilio Ziniades <emilioziniades@gmail.com>
2024-10-30 08:57:22 +0200

    nix: slightly restructure home manager modules

--- nix/modules/home-manager/default.nix
+++ nix/modules/home-manager/default.nix
@@ -1,16 +1,13 @@
 {
   imports = [
+    ./common.nix
     ./neovim.nix
     ./tmux.nix
     ./git.nix
     ./wezterm.nix
     ./tms.nix
-    ./programming-languages.nix
+    ./programming.nix
     ./vt.nix
     ./gui.nix
-    # TODO: these are not enable-able
-    ./shells.nix
-    ./core-utils.nix
-    ./nix.nix
   ];
 }
