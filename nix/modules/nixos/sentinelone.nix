c89fa496307332d19fc66f9c59388218e83ede8b
Emilio Ziniades <emilioz@za.velocitytrade.com>
2024-11-22 12:54:13 +0200

    nix: turn on sentinelone module for kayak and get it working

--- nix/modules/nixos/sentinelone.nix
+++ nix/modules/nixos/sentinelone.nix
@@ -1,24 +1,19 @@
 {
   pkgs,
   config,
-  sentinelone,
-  dotfiles-secrets,
+  inputs,
   ...
-}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      sentinelone = sentinelone.packages.${pkgs.system}.sentinelone.overrideAttrs (finalAttrs: prevAttrs: {
-        src = "${dotfiles-secrets}/files/SentinelAgent_linux_x86_64_v24_2_1_8.deb";
-      });
-    })
-  ];
-
-  environment.systemPackages = [pkgs.sentinelone];
-
+}: let
+  sentinelone = inputs.sentinelone.packages.${pkgs.system}.sentinelone.overrideAttrs (finalAttrs: prevAttrs: {
+    src = "${inputs.dotfiles-secrets}/files/SentinelAgent_linux_x86_64_v24_2_1_8.deb";
+  });
+in {
   services.sentinelone.enable = true;
+  services.sentinelone.package = sentinelone;
   services.sentinelone.email = "emilioz@za.velocitytrade.com";
   services.sentinelone.serialNumber = "0";
   services.sentinelone.sentinelOneManagementTokenPath = config.age.secrets.sentinelone-management-token.path;
 
-  age.secrets.sentinelone-management-token.file = "${dotfiles-secrets}/secrets/sentinelone-management-token.age";
+  age.secrets.sentinelone-management-token.file = "${inputs.dotfiles-secrets}/secrets/sentinelone-management-token.age";
+  age.identityPaths = ["/home/emilioz/.ssh/id_rsa"];
 }
