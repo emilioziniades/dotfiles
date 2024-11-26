130c264544b698c9ad22feca6159b6553b727bc6
Emilio Ziniades <emilioziniades@gmail.com>
2024-11-26 21:56:01 +0200

    nix: install wezterm on macos again!

--- nix/modules/home-manager/wezterm.nix
+++ nix/modules/home-manager/wezterm.nix
@@ -10,13 +10,7 @@ in {
   options.ez.programs.wezterm.enable = mkEnableOption "wezterm";
 
   config = mkIf cfg.enable {
-    # TODO: wezterm is currently broken on macos
-    # so I'm installing it with brew until this is fixed
-    # see: https://github.com/NixOS/nixpkgs/issues/239384
-    home.packages =
-      if pkgs.stdenv.isLinux
-      then [pkgs.wezterm]
-      else [];
+    home.packages = [pkgs.wezterm];
 
     xdg.configFile.wezterm = {
       source = ../../../wezterm;
