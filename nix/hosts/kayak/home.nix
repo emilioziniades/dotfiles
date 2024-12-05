965f12a19724900ced121f8526b6be94faaecf8d
Emilio Ziniades <emilioziniades@protonmail.com>
2024-12-05 18:02:06 +0200

    nix: turn on dotnet again for kayak

--- nix/hosts/kayak/home.nix
+++ nix/hosts/kayak/home.nix
@@ -26,7 +26,7 @@
   ez.programming = {
     python.enable = true;
     typescript.enable = true;
-    dotnet.enable = false;
+    dotnet.enable = true;
     lua.enable = true;
     nix.enable = true;
     html.enable = false;
