022c457c502bdabc6a0fe3de3c1429e09b8eae78
Emilio Ziniades <emilioziniades@protonmail.com>
2025-08-12 12:35:41 +0200

    nix: point nix-darwin at new repo url

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     darwin = {
-      url = "github:lnl7/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
