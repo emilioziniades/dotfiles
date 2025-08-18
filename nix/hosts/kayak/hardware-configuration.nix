58fc7d5d4c3c65e17b3c48d514d883039019b1fe
Emilio Ziniades <emilioziniades@protonmail.com>
2025-08-18 16:21:47 +0200

    nix(kayak): fix hardware-configuration.nix

--- nix/hosts/kayak/hardware-configuration.nix
+++ nix/hosts/kayak/hardware-configuration.nix
@@ -14,7 +14,7 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b14d7962-9b36-448f-ac68-781ad98c5440";
+    { device = "/dev/mapper/luks-6a709370-34a6-42c4-9614-1669840a3a01";
       fsType = "ext4";
     };
 
@@ -26,16 +26,7 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/1ec0d7b4-90bd-4aba-90a5-e0334d18f3f7"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
