833bf89681a4095b2c957e4efc31de79cb87c4b9
Emilio Ziniades <emilioziniades@protonmail.com>
2026-02-19 16:22:19 +0200

    nix(taskwarrior): add `task byproject` custom report

--- nix/modules/home-manager/taskwarrior.nix
+++ nix/modules/home-manager/taskwarrior.nix
@@ -23,6 +23,12 @@ in
       news.version=${lib.getVersion pkgs.taskwarrior3}
       data.location=~/.local/share/task
       hooks.location=~/.config/task/hooks
+
+      # custom report to list tasks by project
+      report.byproject.description=Tasks grouped by project
+      report.byproject.columns=id,project,priority,description
+      report.byproject.sort=project+,priority-
+      report.byproject.filter=status:pending
     '';
 
   };
