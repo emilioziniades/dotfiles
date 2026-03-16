69bae8b3d72755407da516f61a026a4d6ff10f23
Emilio Ziniades <emilioziniades@protonmail.com>
2026-03-16 10:15:56 +0200

    nvim: configure oil

--- nvim/init.lua
+++ nvim/init.lua
@@ -774,7 +774,11 @@ require("lazy").setup({
 	-- oil
 	{
 		"stevearc/oil.nvim",
-		opts = {},
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		lazy = false,
 	},
