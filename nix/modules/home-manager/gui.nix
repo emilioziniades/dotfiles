166987f9bc77f739afc7556982edadac0a1cdaf2
Emilio Ziniades <emilioziniades@protonmail.com>
2026-04-08 08:48:46 +0200

    nix: use older version of electron for obsidian

--- nix/modules/home-manager/gui.nix
+++ nix/modules/home-manager/gui.nix
@@ -16,7 +16,10 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       firefox
-      obsidian
+      # TODO: set below back to `obsidian` after this issue is resolved:
+      # - https://github.com/NixOS/nixpkgs/issues/505078
+      # - https://github.com/NixOS/nixpkgs/pull/505535
+      (obsidian.override { electron = electron_39; })
       teams-for-linux
       spotify
       discord
