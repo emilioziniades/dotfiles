be85f390dc2e905e307923bb58116bfb96285a57
Emilio Ziniades <emilioziniades@protonmail.com>
2026-05-28 16:04:41 +0200

    nix: icon for CC notifications

--- nix/modules/home-manager/claude.nix
+++ nix/modules/home-manager/claude.nix
@@ -8,6 +8,11 @@ with lib;
 let
   cfg = config.ez.programs.claude;
 
+  claude-icon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-svg/icons/claudecode-color.svg";
+    sha256 = "0zw7d81xgbm884skyh0rrd9j0pnba6fsaqkyiynia24xfj6ks2v7";
+  };
+
   claude-notify =
     with pkgs;
     symlinkJoin rec {
@@ -31,7 +36,7 @@ let
             title="Terminal"
         fi
 
-        notify-send --app-name "Claude Code" --icon "utilities-terminal" "$title" "$msg"
+        notify-send --app-name "Claude Code" --icon "${claude-icon}" "$title" "$msg"
       '';
 
     };
