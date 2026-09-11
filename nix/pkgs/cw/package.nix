c867a4ab300e9de1f6bcef6f66ba9fa01564a676
Emilio Ziniades <emilioziniades@protonmail.com>
2026-09-11 12:07:20 +0200

    update cw package

--- nix/pkgs/cw/package.nix
+++ nix/pkgs/cw/package.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "cw";
-  version = "0.1.1";
+  version = "0.1.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "emilioziniades";
     repo = "cw";
     rev = finalAttrs.version;
-    hash = "sha256-G0iotpsy9qnoJIT/vlRMjWRGTMAMvi4u4HJuWpBhoc4=";
+    hash = "sha256-IvBePMz6+zEXZIvaOlv6nS4Wc2+5XQBX0r87TBgFGSU=";
   };
 
   build-system = with python3Packages; [
