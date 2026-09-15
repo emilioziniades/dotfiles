cf667b1b38289f753f15bb02d83eac2059d9461d
Emilio Ziniades <emilioziniades@protonmail.com>
2026-09-15 12:08:07 +0200

    update obsidian overlay again

--- nix/overlays/obsidian.nix
+++ nix/overlays/obsidian.nix
@@ -1,6 +1,7 @@
 # TODO: watch for issues on nixos github and obsidian discourse and remove when fixed
 final: prev: {
   obsidian = prev.obsidian.override {
-    makeDesktopItem = args: final.makeDesktopItem (args // { startupWMClass = "md.Obsidian"; });
+    makeDesktopItem =
+      args: final.makeDesktopItem (args // { startupWMClass = "md.obsidian.Obsidian"; });
   };
 }
