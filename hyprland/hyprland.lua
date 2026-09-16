c2008c6fcb1f8249cb37bfd076e751002b913470
Emilio Ziniades <emilioziniades@protonmail.com>
2026-09-16 15:57:13 +0200

    switch hyprland config to ghostty

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -33,7 +33,7 @@ hl.monitor({
 ---------------------
 
 -- Set programs that you use
-local terminal = "kitty"
+local terminal = "ghostty"
 local fileManager = "dolphin"
 local menu = "hyprlauncher"
 
