1947d44cd228395db2194a5b9e224340ad56da5b
Emilio Ziniades <emilioziniades@protonmail.com>
2026-09-22 14:27:43 +0200

    nix: revert all gnome plugins and customization

--- nix/modules/nixos/gnome.nix
+++ nix/modules/nixos/gnome.nix
@@ -1,14 +1,6 @@
 {
-  pkgs,
-  ...
-}:
-{
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
-
-  environment.systemPackages = [
-    pkgs.gnomeExtensions."hide-top-bar"
-  ];
 
   programs.dconf = {
     enable = true;
@@ -23,19 +15,10 @@
             xkb-options = [ "caps:escape" ];
           };
 
-          "org/gnome/shell" = {
-            enabled-extensions = [ pkgs.gnomeExtensions."hide-top-bar".extensionUuid ];
-          };
-
           "org/gnome/shell/keybindings" = {
             show-screenshot-ui = [ "Insert" ];
           };
 
-          "org/gnome/shell/extensions/hidetopbar" = {
-            shortcut-keybind = [ "<Super>b" ];
-            shortcut-delay = 0.0;
-            shortcut-toggles = true;
-          };
         };
 
         locks = [
