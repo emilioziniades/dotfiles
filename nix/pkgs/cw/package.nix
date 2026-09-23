4a59e5538d6dd2182677d4ae8ad658a1aec0ce4a
Emilio Ziniades <emilioziniades@protonmail.com>
2026-09-23 16:11:39 +0200

    nixosConfigurations.kayak.pkgs.cw: 0.1.3 -> 0.1.4
    Diff: https://github.com/emilioziniades/cw/compare/0.1.3...0.1.4

--- nix/pkgs/cw/package.nix
+++ nix/pkgs/cw/package.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "cw";
-  version = "0.1.3";
+  version = "0.1.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "emilioziniades";
     repo = "cw";
     rev = finalAttrs.version;
-    hash = "sha256-RTuM827pBUGbDtuJymgFwVpyq0wCeC4IvyY9IbGBTos=";
+    hash = "sha256-44zaE3LNI3nLh5CUCTpVRyyOAiNb2hv3Z3e8kjKEe3o=";
   };
 
   build-system = with python3Packages; [
